25156919e9d0227ca8aa1eabe1bc038a95d7b2be
jayjayhust <jayhust@163.com>
2026-02-09 15:20:01 +0800

    modify reward for section 2-2

--- motrix_envs/src/motrix_envs/navigation/vbot/cfg.py
+++ motrix_envs/src/motrix_envs/navigation/vbot/cfg.py
@@ -511,8 +511,8 @@ class VBotSection012EnvCfg(VBotStairsEnvCfg):
     """VBot Section012单独训练配置 - 02地形"""
     # model_file: str = os.path.dirname(__file__) + "/xmls/scene_section012.xml"
     model_file: str = os.path.dirname(__file__) + "/xmls/scene_section012-1.xml"
-    max_episode_seconds: float = 40.0  # 拉长一倍：从20秒增加到40秒
-    max_episode_steps: int = 4000  # 拉长一倍：从2000步增加到4000步（本回合步数到达后会重新开始训练）
+    max_episode_seconds: float = 40.0  # 地形复杂：从20秒增加到40秒
+    max_episode_steps: int = 4000  # 地形复杂：从2000步增加到4000步（本回合步数到达后会重新开始训练）
     sim_dt: float = 0.01    # 仿真步长 10ms = 100Hz
     ctrl_dt: float = 0.01
     reset_yaw_scale: float = 0.1
@@ -542,23 +542,24 @@ class VBotSection012EnvCfg(VBotStairsEnvCfg):
     class InitState:
         # 起始位置：随机化范围内生成
         pos = [0.0, 7.5, 2.0]  # 机器人初始出生的中心位置（平地过渡区）
+        # pos = [-3.0, 7.5, 2.0]  # 机器人初始出生的中心位置（平地过渡区,靠近吊桥侧）
         # pos = [0.0, 10, 2.0]  # 机器人初始出生的中心位置（波浪型区域）
         pos_range = 0.1  # 位置随机范围：±0.1m（0.2m×0.2m区域）
         pos_randomization_range = [-5.0, -5.0, 5.0, 5.0]  # X±5.0m, Y±5.0m随机
 
         default_joint_angles = {
-            "FR_hip_joint": -0.0,
-            "FR_thigh_joint": 0.9,
-            "FR_calf_joint": -1.8,
-            "FL_hip_joint": 0.0,
-            "FL_thigh_joint": 0.9,
-            "FL_calf_joint": -1.8,
-            "RR_hip_joint": -0.0,
-            "RR_thigh_joint": 0.9,
-            "RR_calf_joint": -1.8,
-            "RL_hip_joint": 0.0,
-            "RL_thigh_joint": 0.9,
-            "RL_calf_joint": -1.8,
+            "FR_hip_joint": -0.0,     # 右前髋关节
+            "FR_thigh_joint": 0.95,   # 右前大腿（适中抬高）
+            "FR_calf_joint": -1.85,   # 右前小腿
+            "FL_hip_joint": 0.0,      # 左前髋关节
+            "FL_thigh_joint": 0.95,   # 左前大腿（适中抬高）
+            "FL_calf_joint": -1.85,   # 左前小腿
+            "RR_hip_joint": -0.0,     # 右后髋关节
+            "RR_thigh_joint": 0.85,   # 右后大腿（稍低提供推进力）
+            "RR_calf_joint": -1.75,   # 右后小腿
+            "RL_hip_joint": 0.0,      # 左后髋关节
+            "RL_thigh_joint": 0.85,   # 左后大腿（稍低提供推进力）
+            "RL_calf_joint": -1.75,   # 左后小腿
         }
 
     @dataclass
